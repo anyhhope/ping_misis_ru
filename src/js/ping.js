63b16e51e6dacfff7eee716f9e8e86cfc1febb65
anyhhope <hopeanysimova@yandex.ru>
2023-05-20 17:19:59 +0300

    add files to table

--- src/js/ping.js
+++ src/js/ping.js
@@ -9,31 +9,48 @@ const fileInput = document.getElementById('csv')
 //   reader.readAsBinaryString(fileInput.files[0])
 // }
 
+
 const readFile = () => {
     const reader = new FileReader()
     reader.onload = () => {
-        document.getElementById('out').innerHTML = reader.result
-        console.log(typeof document.getElementById('out').innerHTML)
+        data = reader.result;
+        var div = document.getElementById('container');
+        var lines = data.split("\n"), output = [], i;
+
+        /* HEADERS */
+        output.push("<tr><th>"
+            + lines[0].slice().split(",").join("</th><th>")
+            + "</th></tr>");
+
+        for (i = 1; i < lines.length; i++)
+            output.push("<tr><td>"
+                + lines[i].slice().split(",").join("</td><td>")
+                + "</td></tr>");
+
+        output = "<table><tbody>"
+            + output.join("") + "</tbody></table>";
+
+        div.innerHTML = output;
     }
     reader.readAsText(fileInput.files[0], 'UTF-8');
-    // start reading the file with a specific encoding. When it is done, calls the onload event defined above.
 }
 
-var div = document.getElementById('container');
-var data = 'heading1,heading2,heading3,heading4,heading5\nvalue1_1,value2_1,value3_1,value4_1,value5_1\nvalue1_2,value2_2,value3_2,value4_2,value5_2';
-var lines = data.split("\n"), output = [], i;
+fileInput.addEventListener('change', readFile)
+// var div = document.getElementById('container');
+// // var data = 'heading1,heading2,heading3,heading4,heading5\nvalue1_1,value2_1,value3_1,value4_1,value5_1\nvalue1_2,value2_2,value3_2,value4_2,value5_2';
+// var lines = data.split("\n"), output = [], i;
 
-/* HEADERS */
-output.push("<tr><th>"
-    + lines[0].slice().split(",").join("</th><th>")
-    + "</th></tr>");
+// /* HEADERS */
+// output.push("<tr><th>"
+//     + lines[0].slice().split(",").join("</th><th>")
+//     + "</th></tr>");
 
-for (i = 1; i < lines.length; i++)
-    output.push("<tr><td>"
-        + lines[i].slice().split(",").join("</td><td>")
-        + "</td></tr>");
+// for (i = 1; i < lines.length; i++)
+//     output.push("<tr><td>"
+//         + lines[i].slice().split(",").join("</td><td>")
+//         + "</td></tr>");
 
-output = "<table><tbody>"
-    + output.join("") + "</tbody></table>";
+// output = "<table><tbody>"
+//     + output.join("") + "</tbody></table>";
 
-div.innerHTML = output;
+// div.innerHTML = output;
