e93f43a53ee207cf604e27f8c45bda8a6dfbe15f
anyhhope <hopeanysimova@yandex.ru>
2023-05-20 19:13:15 +0300

    charts

--- src/js/ping.js
+++ src/js/ping.js
@@ -1,31 +1,57 @@
+//charts
+import Chart from 'chart.js/auto'
 
-//read file by btn
-const fileInput = document.getElementById('csv')
-const readFile = () => {
-    const reader = new FileReader()
-    reader.onload = () => {
-        data = reader.result;
-        var div = document.querySelector('.for-table');
-        var lines = data.split("\n");
-        let output = [];
-        let i;
-
-        /* HEADERS */
-        output.push("<tr><th>"
-            + lines[0].slice().split(",").join("</th><th>")
-            + "</th></tr>");
-
-        for (i = 1; i < lines.length; i++)
-            output.push("<tr><td>"
-                + lines[i].slice().split(",").join("</td><td>")
-                + "</td></tr>");
-
-        output = "<table><tbody>"
-            + output.join("") + "</tbody></table>";
-
-        div.innerHTML = output;
-    }
-    reader.readAsText(fileInput.files[0], 'UTF-8');
+function makeBar(cnv, type, data){
+    new Chart(cnv, {
+        type: type,
+        data: data
+    });
 }
 
-fileInput.addEventListener('change', readFile)
+const ctx = document.getElementById('myChart');
+
+  
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+    }]
+    },
+    options: {
+    scales: {
+        y: {
+        beginAtZero: true
+        }
+    }
+    }
+});
+
+
+const ctx2 = document.getElementById('myChart2');
+
+const pie = {
+    labels: [
+      'Red',
+      'Blue',
+      'Yellow'
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [300, 50, 100],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
+      ],
+      hoverOffset: 4
+    }]
+  };
+
+new Chart(ctx2, {
+    type: 'doughnut',
+    data: pie,
+});
