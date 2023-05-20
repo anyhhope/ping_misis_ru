03a14a7a343589d90a51f8e1a8e4352844ae86e9
Calliestess <90801166+Calliestess@users.noreply.github.com>
2023-05-21 02:37:33 +0300

    do

--- src/js/forchart.js
+++ src/js/forchart.js
@@ -48,50 +48,14 @@ new Chart(ctx, {
     }
 });
 
-
-// const ctx = document.getElementById('myChart');
-
-// const data_bar = response.data;
-
-// new Chart(ctx, {
-//   type: 'bar',
-//   data: data_bar,
-//   options: {
-//     scales: {
-//       y: {
-//         beginAtZero: true
-//       }
-//     }
-//   }
+// const axios = require('axios');
+// axios.get('http://46.138.243.191:54002/data_bar').then(response => {
+//   // console.log(response.data.datasets[0].data);
+//   bar_resp = response.data.datasets[0].data;
+//   make_bar(bar_resp);
+//   // console.log(response.data);
+//   // console.log(response.data.labels);
+// }).catch(error => {
+//   console.log(error.message);
 // });
-
-let bar_resp;
-
-function make_bar(bar_resp) {
-  let ctx = document.querySelector('#myChart');
-  console.log(bar_resp);
-
-  new Chart(ctx, {
-    type: 'bar',
-    data: [1,2,2,3],
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
-}
-
-const axios = require('axios');
-axios.get('http://46.138.243.191:54002/data_bar').then(response => {
-  // console.log(response.data.datasets[0].data);
-  bar_resp = response.data.datasets[0].data;
-  make_bar(bar_resp);
-  // console.log(response.data);
-  // console.log(response.data.labels);
-}).catch(error => {
-  console.log(error.message);
-});
 
