051629065566caa3f4d9f5625ce9a90084c4dc8a
Calliestess <90801166+Calliestess@users.noreply.github.com>
2023-05-21 03:07:39 +0300

    dhdfj

--- src/js/forchart.js
+++ src/js/forchart.js
@@ -1,56 +1,8 @@
 import Chart from 'chart.js/auto'
-
-const ctx2 = document.getElementById('myChart2'); //pie
-
-const pie = { //man/woman
-    labels: [ //need
-        'Мужчины',
-        'Женщины'
-    ],
-    datasets: [{
-        label: 'My First Dataset',
-        data: [555, 115], //need
-        backgroundColor: [
-            'rgb(54, 162, 235)',
-            'rgb(255, 99, 132)'
-        ],
-        hoverOffset: 4
-    }]
-};
-
-function make_pie(cnv, pie){
-    let pie_chart = new Chart(cnv, {
-        type: 'doughnut',
-        data: pie,
-    });
-}
-
-make_pie(ctx2, pie)
-
-
-
-// document.getElementById("myChart2").onclick = function (evt) {
-//     const points = pie_chart.getElementsAtEventForMode(evt, 'nearest', { intersect: true }, true);
-
-//     if (points.length) {
-//         const firstPoint = points[0];
-//         const label = pie_chart.data.labels[firstPoint.index];
-//         const value = pie_chart.data.datasets[firstPoint.datasetIndex].data[firstPoint.index];
-//         let h = 0;
-//     }
-// }
+const axios = require('axios');
 
 const ctx = document.getElementById('myChart');
-
-const data_bar = { //для колва людей в категотирии
-    labels: ['Red', 'Blue', 'dfgbh', 'Green', 'dfvg', 'Orange'], //need
-    datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 1234, 23, 2, 3], //need
-        borderWidth: 1
-    }]
-}
-
+let data_bar;
 
 function make_bar(ctx, data_bar){
     let bar_char = new Chart(ctx, {
@@ -66,17 +18,43 @@ function make_bar(ctx, data_bar){
     });
 }
 
-make_bar(ctx, data_bar);
+axios.get('http://46.138.243.191:54002/data_bar').then(response => {
+  data_bar = response.data;
+  make_bar(ctx, data_bar); 
+}).catch(error => {
+  console.log(error.message);
+});
 
+// PIE
+const ctx2 = document.getElementById('myChart2'); 
 
+// const pie = { //man/woman
+//     labels: [ //need
+//         'Man',
+//         'Women'
+//     ],
+//     datasets: [{
+//         label: 'My First Dataset',
+//         data: [555, 115], 
+//         backgroundColor: [
+//             'rgb(54, 162, 235)',
+//             'rgb(255, 99, 132)'
+//         ],
+//         hoverOffset: 4
+//     }]
+// };
+let pie;
+function make_pie(ctx2, pie){
+    let pie_chart = new Chart(ctx2, {
+        type: 'doughnut',
+        data: pie,
+    });
+}
+axios.get('http://46.138.243.191:54002/pie').then(response => {
+//   console.log(response.data);
+  pie = response.data;
+  make_pie(ctx2, pie); 
+}).catch(error => {
+  console.log(error.message);
+});
 
-// document.getElementById("myChart3").onclick = function (evt) {
-//     const points = scatter_chart.getElementsAtEventForMode(evt, 'nearest', { intersect: true }, true);
-
-//     if (points.length) {
-//         const firstPoint = points[0];
-//         const label = scatter_chart.data.labels[firstPoint.index];
-//         const value = scatter_chart.data.datasets[firstPoint.datasetIndex].data[firstPoint.index];
-//         let h = 0;
-//     }
-// }
